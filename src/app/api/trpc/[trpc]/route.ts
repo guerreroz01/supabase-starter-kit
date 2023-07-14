480238040caf1b5b5dd76347ab4bf5812d0666df
oliver <oliver.dentistry@gmail.com>
2023-07-14 13:28:27 +0200

    supabase terminado10

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -13,7 +13,10 @@ async function handler(req: Request, res: Response) {
     createContext: (
       opts: FetchCreateContextFnOptions
     ): object | Promise<object> => {
-      opts.resHeaders.set("Access-Control-Allow-Origin", "*");
+      opts.resHeaders.set(
+        "Access-Control-Allow-Origin",
+        "https://supabase-starter-kit-five.vercel.app"
+      );
       opts.resHeaders.set("Access-Control-Request-Method", "*");
       opts.resHeaders.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
       opts.resHeaders.set("Access-Control-Allow-Headers", "content-type");
